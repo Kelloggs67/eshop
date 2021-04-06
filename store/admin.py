1f08ce3aef677bfabdee9181f9976846f68a5472
Kelloggs67 <timkhutch@gmail.com>
2021-04-06 15:26:33 -0400

    products displayed

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+
 from .models import Category, Product
+
 # Register your models here.
 
 @admin.register(Category)
@@ -9,7 +11,8 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'price', 'in_stock', 'created', 'updated']
-    list_filter = ['in_stock',]
+    list_display = ['title', 'author', 'slug', 'price',
+                    'in_stock', 'created', 'updated']
+    list_filter = ['in_stock', 'is_active']
     list_editable = ['price', 'in_stock']
-    prepopulated_fields = {'slug': ('name',)}
+    prepopulated_fields = {'slug': ('title',)}
